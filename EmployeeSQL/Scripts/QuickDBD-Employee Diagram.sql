39dd251da32da515b49b09d42843a6bbe33dda28
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-08-03 18:10:11 -0700

    added primary keys to composite tables

--- EmployeeSQL/Scripts/QuickDBD-Employee Diagram.sql
+++ EmployeeSQL/Scripts/QuickDBD-Employee Diagram.sql
@@ -34,17 +34,26 @@ CREATE TABLE "employees" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL
+    "dept_no" varchar   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "dept_manager" (
     "dept_no" varchar   NOT NULL,
-    "emp_no" int   NOT NULL
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
